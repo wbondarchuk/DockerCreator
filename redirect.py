6b4573cfacdf3329a86dde77932dc1c684a42e72
wbondarchuk <55180931+wbondarchuk@users.noreply.github.com>
2022-03-19 20:11:23 +0700

    Update redirect.py

--- redirect.py
+++ redirect.py
@@ -1,13 +1,37 @@
 import sys
 import subprocess
+import random
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-def bash(port):
+def port_generator():
+    port = random.choice(ports)
+    return port
+
+
+def port_check(port, work_ports):
+    if port not in work_ports:
+        work_ports.append(port)
+    else:
+        port = port_generator()
+        port_check(port, work_ports)
+    return port
+
+
+def docker_run(port):
     cmd = f'docker run -d --name docker_theia{port} -p {port}:{port} -v "$(pwd):/home/project:cached" elswork/theia'
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    result = p.communicate()[0].decode("utf-8")[:-1]
+    print(result)
+    return result
+
+
+def docker_remove(id):
+    cmd = f'docker rm -f {id}'
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     result = p.communicate()[0]
     print(result)
+    return result
 
 
 class Redirect(BaseHTTPRequestHandler):
@@ -19,15 +43,23 @@ class Redirect(BaseHTTPRequestHandler):
 
 def main():
     server = HTTPServer(("", int(sys.argv[1])), Redirect)
-    bash(3000)
+    docker_id = docker_run(3000)
     print("Forwarding...")
-    print('||',sys.argv[0], '||', int(sys.argv[1]), '||', sys.argv[2], '||')
-    server.serve_forever()
-    server.server_close()
-    print("Server stopped!")
+    print('||', sys.argv[0], '||', int(sys.argv[1]), '||', sys.argv[2], '||')
+    print(docker_id)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        docker_remove(docker_id)
+        print("Server stopped!")
+        server.server_close()
 
 
 if __name__ == '__main__':
+
+    ports = [3000, 3001, 3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010]
+    work_ports = []
+
     if len(sys.argv) - 1 != 2:
         print(f"Usage: {sys.argv[0]} <port_number> <url>")
         sys.exit()
